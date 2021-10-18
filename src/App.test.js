e9ba48e384ae4c1c2c8624e5c972aa5579540c7f
Ivan Lim <limivanb@gmail.com>
2021-10-18 11:02:58 +0800

    initial project template

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,12 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();  
 });
+
+test('renders learn react reload', () => {
+  render(<App />);
+  const linkElement2 = screen.getByText(/reload/i);
+  expect(linkElement2).toBeInTheDocument()
+});
+
---
+++ src/components/MainContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function MainContent() {
+  return (
+    <div>
+      <h3>Sample text</h3>
+      <Button variant="contained">Sample Button</Button>
+    </div>
+  );
+}
+
+export default MainContent;
